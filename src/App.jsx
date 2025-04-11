91fc530431e71251d46cdfaa787458cee7eb6623
nikita <nmensky@gmail.com>
2025-04-11 18:56:57 +0300

    rew to create-element year

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import {useState} from 'react'
+import { createElement, useState } from 'react'
 import './App.css'
 
-export const App = () => {
-    const [count, setCount] = useState(0)
-    const currentYear = new Date().getFullYear() // императивный подход
-
-    return (
-        <>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}> {/*императивный подход*/}
-                    count is {count}
-                </button>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-            <footer>
-                <p>Current Year: {currentYear}</p> {/*декларативный подход*/}
-            </footer>
-        </>
-    )
+function CurrentYear() {
+    const currentYear = new Date().getFullYear(); // императивный подход
+    return createElement('p', null, `Current Year: ${currentYear}`); // декларативный подход
 }
 
+export const App = () => {
+    const [count, setCount] = useState(0);
+
+    return createElement(
+        'div',
+        null,
+        createElement('h1', null, 'Vite + React'),
+        createElement(
+            'div',
+            { className: 'card' },
+            createElement(
+                'button',
+                { onClick: () => setCount((count) => count + 1) }, // императивный подход
+                `count is ${count}`
+            )
+        ),
+        createElement(
+            'p',
+            { className: 'read-the-docs' },
+            'Click on the Vite and React logos to learn more'
+        ),
+        createElement(CurrentYear)
+    )
+}
 
